dba7cb8511a075405ae45816cb08f1a734a8b73f
Maryam Afshar <mary1afshar@gmail.com>
2023-06-10 20:56:31 -0700

    Initial Commit

---
+++ RideBetter/RideBetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideBetterApp.swift
+//  RideBetter
+//
+//  Created by Maryam Afshar on 2023-06-10.
+//
+
+import SwiftUI
+
+@main
+struct RideBetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
